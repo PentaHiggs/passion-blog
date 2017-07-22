2edc458bcaaa032652e5196e443be05e63124d14
Andy Perez <ichiroska89@yahoo.com>
2017-07-22 00:16:32 -0400

    None of the models besides BlogPost were registered in admin.py, making it impossible to write a BlogPost since it had Author as a primary key, but no way to create new Author objects.  This has been remidied

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from blog.models import BlogPost
+from blog.models import Author
+from blog.models import BlogPostCategory
+from blog.models import BlogComment
 
 # Register your models here.
 
 admin.site.register(BlogPost)
+admin.site.register(Author)
+admin.site.register(BlogPostCategory)
+admin.site.register(BlogComment)
